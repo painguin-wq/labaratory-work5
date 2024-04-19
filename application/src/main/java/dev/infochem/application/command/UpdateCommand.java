f62bc728688c98a793a6bc1c972934ca369958aa
painguin-wq <alina.infochem@niuitmo.ru>
2024-04-19 15:21:14 +0300

    update "update" command

--- application/src/main/java/dev/infochem/application/command/UpdateCommand.java
+++ application/src/main/java/dev/infochem/application/command/UpdateCommand.java
@@ -47,37 +47,102 @@ public class UpdateCommand extends DefaultCommand {
                     field = Flat.class.getDeclaredField(scanner.nextCommand());
                 } catch (NoSuchFieldException e) {
                     System.out.println("Данный элемент не был найден");
+                    System.out.println(listOfElements);
                 }
             }
-            System.out.printf("Enter new value of field \"%s\": ", field.getName());
             field.setAccessible(true);
-            String newValue = scanner.nextCommand();
             Class<?> fieldType = field.getType();
-            if (fieldType == String.class) {
-                field.set(flat, newValue);
-            } else if (fieldType == Long.class || fieldType == long.class) {
-                setLong(newValue, field, flat);
-            } else if (fieldType == Float.class || fieldType == float.class) {
-                setFloat(newValue, field, flat);
+            if (field.getName().equals("name")) {
+                flat.setName(inputRequest(() -> {
+                    String name = scanner.nextCommand();
+                    if (name == null || name.isEmpty()) {
+                        throw new IllegalArgumentException("Имя поля должно быть не пустым");
+                    }
+                    return name;
+                }, "Введите новое имя квартиры: ", "Введено невалидное имя, попробуйте еще раз."));
+            } else if (field.getName().equals("numberOfRooms")) {
+                flat.setNumberOfRooms(inputRequest(() -> {
+                    String numberOfRooms = scanner.nextCommand();
+                    if (!numberOfRooms.isEmpty()) {
+                        Long rooms = Long.parseLong(numberOfRooms);
+                        if (rooms < 0) {
+                            throw new IllegalArgumentException("Количество комнат должно быть больше 0");
+                        }
+                        return rooms;
+                    } return null;
+                }, "Введите новое количество комнат (Если хотите пропустить введите пустую строку): ", "Количество комнат должно быть числом."));
+            } else if (field.getName().equals("numberOfBathrooms")) {
+                flat.setNumberOfBathrooms(inputRequest(() -> {
+                    Long numberOfBathrooms = scanner.nextLong();
+                    if (numberOfBathrooms < 0) {
+                        throw new IllegalArgumentException("Количество ванных комнат должно быть больше 0");
+                    }
+                    return numberOfBathrooms;
+                },"Введите новое количество ванных комнат: ", "Количество ванных комнат должно быть числом"));
+
+            } else if (field.getName().equals("timeToMetroByTransport")) {
+                flat.setTimeToMetroByTransport(inputRequest(() ->  {
+                    float timeToMetroByTransport = scanner.nextFloat();
+                    if (timeToMetroByTransport < 0) {
+                        throw new IllegalArgumentException("Значение времени до метро на транспорте должно быть больше 0");
+                    }
+                    return timeToMetroByTransport;
+                }, "Введите новое время до метро на транспорте: ", "Время до метро на транспорте должно быть числом."));
+
+            } else if(field.getName().equals("area")) {
+                flat.setArea(inputRequest(() -> {
+                    Float area = scanner.nextFloat();
+                    if (area < 0) {
+                        throw new IllegalArgumentException("Площадь поля должна быть больше 0");
+                    }
+                    return area;
+                }, "Введите новую площадь квартиры: ", "Площадь квартиры должна быть числом"));
             } else if (fieldType == Coordinates.class) {
+                final String coordinatesInputError = "Координата должна быть числом!";
                 Coordinates coordinates = flat.getCoordinates();
-                System.out.println("Enter the x coordinate: ");
-                coordinates.setX(Long.parseLong(scanner.nextCommand()));
-                System.out.println("Enter the y coordinate: ");
-                coordinates.setY(Integer.parseInt(scanner.nextCommand()));
+                System.out.println("Введите новою координату x: ");
+                coordinates.setX(inputRequest(() -> {
+                    Long xCoordinate = scanner.nextLong();
+                    if (xCoordinate == null || xCoordinate < -336) {
+                        throw new IllegalArgumentException("Поле x должно быть не пустым и превышать -336");
+                    }
+                    return xCoordinate;
+                }, "Введите координату x: ", coordinatesInputError));
+                coordinates.setY(inputRequest(scanner::nextInt, "Введите координату y: ", coordinatesInputError));
             } else if (fieldType == House.class) {
                 House house = flat.getHouse();
-                System.out.println("Enter the name of the house: ");
-                house.setName(scanner.nextCommand());
-                System.out.println("Enter the year of the house: ");
-                house.setYear(Integer.parseInt(scanner.nextCommand()));
-                System.out.println("Enter the number of lifts in the house: ");
-                house.setNumberOfLifts(Integer.parseInt(scanner.nextCommand()));
-            } else if (fieldType == Furnish.class) {
-                System.out.println("Enter furnish (Available variants: NONE, BAD, LITTLE): ");
-                flat.setFurnish(Furnish.valueOf(scanner.nextCommand()));
-            }
+                house.setName(inputRequest(() -> {
+                    String name = scanner.nextCommand();
+                    if (!name.isEmpty()) {
+                        return name;
+                    } return null;
+                }, "Введите новое имя дома (Если хотите пропустить введите пустую строку): ", "Возникла ошибка, попробуйте еще раз"));
 
+                house.setYear(inputRequest(() -> {
+                    Integer year = scanner.nextInt();
+                    if (year < 0) {
+                        throw new IllegalArgumentException("Год постройки должен быть больше нуля");
+                    }
+                    return year;
+                }, "Введите новый год постройки дома: ", "Год постройки должен быть числом"));
+
+                house.setNumberOfLifts(inputRequest(() -> {
+                    Integer numberOfLifts = scanner.nextInt();
+                    if (numberOfLifts < 0) {
+                        throw new IllegalArgumentException("Количество лифтов должно быть больше нуля");
+                    } return numberOfLifts;
+                }, "Введите новое количество лифтов: ", "Количество лифтов должно быть числом."));
+            } else if (fieldType == Furnish.class) {
+                flat.setFurnish(inputRequest(() -> {
+                            String furnish = scanner.nextCommand();
+                            try {
+                                return Furnish.valueOf(furnish);
+                            } catch (IllegalArgumentException ex) {
+                                throw new UnsupportedOperationException("Доступны только варианты: NONE, BAD, LITTLE!");
+                            }
+                        }, "Введите новую мебель (Доступные варианты: NONE, BAD, LITTLE): ", "Доступны только варианты: NONE, BAD, LITTLE!"
+                ));
+            }
         } else {
             System.out.printf("Не удается найти элемент с id = %d%n", id);
         }
